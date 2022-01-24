2aa17d480e21fc46ba60b6d95c83fdf3e291a681
Loiko <bohdan.loiko@gmail.com>
2022-01-24 14:15:11 +0200

    enable cross orinin in filter

---
+++ src/main/java/com/restaurant/web/filters/MyProjectFilter.java
@@ -0,0 +1,25 @@
+package com.restaurant.web.filters;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class MyProjectFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,PUT,OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Max-Age", "86400");
+        chain.doFilter(req, res);
+    }
+}
