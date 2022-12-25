0e2d9743b9dc2cedb00046e4c22b8c4cbf469290
Bohdan Loiko <bohdan.loiko@gmail.com>
2022-12-25 17:08:31 +0200

    Create new endpoint Post /menu1 and / GetMenuPage

--- src/test/java/com/restaurant/web/MenuControllerIT.java
+++ src/test/java/com/restaurant/web/MenuControllerIT.java
@@ -3,6 +3,8 @@ package com.restaurant.web;
 import com.restaurant.RestaurantApplication;
 import com.restaurant.database.dao.FoodRepository;
 import com.restaurant.database.entity.FoodItem;
+import com.restaurant.database.entity.MenuPage;
+import com.restaurant.web.dto.GetMenuResponse;
 import com.restaurant.web.dto.MenuFilterBy;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
@@ -12,11 +14,14 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -59,6 +64,63 @@ class MenuControllerIT {
                 Arguments.of(SNACKS),
                 Arguments.of(DESSERTS)
         );
+    }
+
+    @Test
+    public void testGetMenuPage() {
+        final int pageSize = 10;
+        Sort.Direction sortDirection = Sort.Direction.DESC;
+        MenuPage menuPage = new MenuPage(1, pageSize, sortDirection, "name", "Desserts");
+
+        GetMenuResponse menuResponse = menuController.getMenuPage(menuPage);
+
+        assertEquals(1, menuResponse.getPage());
+        List<FoodItem> actualFoodItems = menuResponse.getFoodItems();
+        List<String> categoryNames = getCategoryNames(actualFoodItems);
+        assertEquals(1, categoryNames.size());
+        assertTrue(categoryNames.contains(DESSERTS.getValue()));
+
+        List<FoodItem> sortedFoodItems = sortItemsByComparator(actualFoodItems, FoodItem::getName, sortDirection);
+        assertEquals(sortedFoodItems, actualFoodItems);
+        assertTrue(menuResponse.getFoodItems().size() <= pageSize);
+    }
+
+    @Test
+    public void testGetMenuPageSortByCategoryDefault() {
+        final int pageSize = 10;
+        Sort.Direction sortDirection = Sort.Direction.ASC;
+        MenuPage menuPage = new MenuPage(1, 3, sortDirection, null, "Desserts");
+
+        GetMenuResponse menuResponse = menuController.getMenuPage(menuPage);
+
+        assertEquals(1, menuResponse.getPage());
+        List<FoodItem> actualFoodItems = menuResponse.getFoodItems();
+        List<String> categoryNames = getCategoryNames(actualFoodItems);
+        assertEquals(1, categoryNames.size());
+
+        List<FoodItem> sortedFoodItems = sortItemsByComparator(actualFoodItems, foodItem -> foodItem.getCategory().getName(), sortDirection);
+        assertEquals(sortedFoodItems, actualFoodItems);
+        assertTrue(menuResponse.getFoodItems().size() <= pageSize);
+    }
+
+    private List<FoodItem> sortItemsByComparator(List<FoodItem> actualFoodItems,
+                                                 Function<? super FoodItem, String> functionCompareBy,
+                                                 Sort.Direction sortOrder) {
+        Comparator<? super FoodItem> comparing = Comparator.comparing(functionCompareBy);
+        if (sortOrder.equals(Sort.Direction.DESC)) {
+            comparing = comparing.reversed();
+        }
+        return actualFoodItems.stream()
+                .sorted(comparing)
+                .collect(Collectors.toList());
+    }
+
+    @NotNull
+    private static List<String> getCategoryNames(List<FoodItem> actualFoodItems) {
+        return actualFoodItems.stream()
+                .map(foodItem -> foodItem.getCategory().getName())
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     private static void assertCategoriesInResponse(List<FoodItem> actualFoodItems, List<String> expectedCategories) {
