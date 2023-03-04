cdcd0a1bacba77a926610c732aa4ad5db9240867
Bohdan Loiko <bohdan.loiko@gmail.com>
2023-03-05 00:12:26 +0200

    add /myorders1 endpoint

---
+++ src/main/java/com/restaurant/web/dto/MyOrdersResponse.java
@@ -0,0 +1,15 @@
+package com.restaurant.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyOrdersResponse {
+
+    private List<MyOrdersDto> myOrders;
+}
